1eb91d14bba96fdfb308106947222a4bf32efd01
KirylKovaliov <kkirusha@gmail.com>
2014-02-22 14:49:25 +0100

    add convertor

---
+++ code/Tests/MediaService.Access.Tests/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MediaService.Access.Tests.ServiceReference1;
+
+namespace MediaService.Access.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ConverterClient client = new ConverterClient();
+            client.DoWork();
+        }
+    }
+}
